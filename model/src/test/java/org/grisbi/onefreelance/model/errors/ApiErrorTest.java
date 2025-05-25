55910e3aa3cfff8a9c75435396f1b96d88a16f7e
gillesandrieu <gilles.andrieu.pro@gmail.com>
2025-05-25 21:27:50 +0200

    fix coverage

--- model/src/test/java/org/grisbi/onefreelance/model/errors/ApiErrorTest.java
+++ model/src/test/java/org/grisbi/onefreelance/model/errors/ApiErrorTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.vavr.control.Option;
 import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 class ApiErrorTest {
 
@@ -31,5 +33,14 @@ class ApiErrorTest {
     assertThat(result.getError()).isEqualTo(errorHandler);
     assertThat(result.getErrorDescription()).isEqualTo(message);
     assertThat(result.getStatus()).isEqualTo(errorHandler.getStatus().value());
+  }
+
+  @Test
+  void forException() {
+    final var errorHandler = new Throwable();
+    final var result = ApiError.forException(errorHandler);
+    assertThat(result.getError()).isEqualTo(ErrorHandler.NOT_FOUND);
+    assertThat(result.getErrorDescription()).isNull();
+    assertThat(result.getStatus()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR.value());
   }
 }
